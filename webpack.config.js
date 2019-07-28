a8ba0fde708435bec55d41c1951bc4978be6f4c7
raytx <470437155@qq.com>
2019-07-29 00:35:20 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack =require('webpack')
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-cheap-module-source-map",
+  entry: {
+    main: "./src/index.js"
+  },
+  devServer: {
+    contentBase: "./dist",
+    open: true,
+    hot: true,
+    hotOnly:true,
+    port: 3000  
+  },
+  module: {
+    rules: [
+      {
+        test:/\.js$/,
+        exclude:/node_module/,
+        loader:"babel-loader"
+      },
+      {
+        test: /\.(png|png|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "[name]_[hash].[ext]",
+            outputPath: "images/",
+            limit: 2048
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|svg|woff)$/,
+        use: {
+          loader: "file-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          "sass-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          "postcss-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  // htmlWebpackPlugin会在打包结束后,自动生成一个html文件,并把打包生成的js自动引入到这个html文件中
+  output: {
+    publicPath:"/",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
